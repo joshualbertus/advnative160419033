f2b887e2987638fd84c9fde3d1519f2751829e71
Joshua Albertus S <joshuasamarianto@gmail.com>
2022-02-17 13:53:59 +0700

    From Class

---
+++ app/src/main/java/id/ubaya/adv160419033week2/MainFragment.kt
@@ -0,0 +1,33 @@
+package id.ubaya.adv160419033week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonStart.setOnClickListener {
+            val playerName = editName.text.toString()
+            val action = MainFragmentDirections.actionGameFragment(playerName)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
